bdb97a101866097963a7bf2fde0698e3cffe929c
bboynam99 <30984515+bboynam99@users.noreply.github.com>
2020-10-05 20:24:51 +0700

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -3,10 +3,10 @@ require('babel-polyfill');
 
 module.exports = {
   networks: {
-    ropsten: {
+    development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "3" // Match any network id
+      network_id: "*" // Match any network id
     },
   },
   contracts_directory: './src/contracts/',
